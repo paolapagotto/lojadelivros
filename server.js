84458fe1e88f539f6fcc46b20ac9f09d093d5e0a
paolapagotto <paolapagotto@gmail.com>
2020-09-13 00:27:35 +0100

    second route

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 const http = require('http');
 const servidor = http.createServer(function(req, resp){
-    resp.end(`    
-    <html>
-        <head>
-            <meta charset = "utf-8">
-        </head>
-        <body>
-            <h1>Hello world </h1>
-        </body>
-    </html>
-    `)
+    let html = '';
+    if(req.url == '/'){
+        html = `    
+        <html>
+            <head>
+                <meta charset = "utf-8">
+            </head>
+            <body>
+                <h1>Casa do Código </h1>
+            </body>
+        </html>
+        `
+    } else if(req.url == '/livros'){
+        html = `    
+        <html>
+            <head>
+                <meta charset = "utf-8">
+            </head>
+            <body>
+                <h1>Listagem de Livros</h1>
+            </body>
+        </html>
+        `
+    }
+    resp.end(html)
 });
 servidor.listen(3000);
