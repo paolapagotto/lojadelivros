105a0ea69bb5dfba634dd02f898e11fdc98d967c
paolapagotto <paolapagotto@gmail.com>
2020-09-13 01:08:09 +0100

    using express as a server

--- server.js
+++ server.js
@@ -1,29 +1,35 @@
-const http = require('http');
-const servidor = http.createServer(function(req, resp){
-    let html = '';
-    if(req.url == '/'){
-        html = `    
-        <html>
-            <head>
-                <meta charset = "utf-8">
-            </head>
-            <body>
-                <h1>Casa do Código </h1>
-            </body>
-        </html>
-        `
-    } else if(req.url == '/livros'){
-        html = `    
-        <html>
-            <head>
-                <meta charset = "utf-8">
-            </head>
-            <body>
-                <h1>Listagem de Livros</h1>
-            </body>
-        </html>
-        `
-    }
-    resp.end(html)
-});
-servidor.listen(3000);
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+app.listen(port, function(){
+    console.log('Servidor rodando: http://localhost:'+port)
+})
+
+app.get('/', function(req, resp){
+    resp.send(`
+    <html>
+        <head>
+            <meta charset = "utf-8">
+        </head>
+        <body>
+            <h1>Casa do Código </h1>
+        </body>
+    </html>
+    `)
+})
+
+
+app.get('/livros', function(req, resp){
+    resp.send(`
+    <html>
+        <head>
+            <meta charset = "utf-8">
+        </head>
+        <body>
+            <h1>Listagem de Livros</h1>
+        </body>
+    </html>
+    `)
+})
