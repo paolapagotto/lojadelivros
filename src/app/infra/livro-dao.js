9393d285e0abba096376c768b7ad3a84607b1f35
paolapagotto <paolapagotto@gmail.com>
2020-09-13 21:17:52 +0100

    manipulating requisition with middlewares

--- src/app/infra/livro-dao.js
+++ src/app/infra/livro-dao.js
@@ -5,6 +5,33 @@ class LivroDao {
     }
 
 
+    adiciona(livro) {
+
+        return new Promise((resolve, reject) => {
+            this._db.run(`
+                INSERT INTO livros (
+                    titulo, 
+                    preco,
+                    descricao
+                ) values(?,?,?)
+                `,
+                [
+                    livro.titulo,
+                    livro.preco,
+                    livro.descricao
+                ],
+
+                (err) => {
+                    if (err) {
+                        console.log(err);
+                        return reject('Não foi possível adicionar o livro!');
+                    }
+                    return resolve();
+                }
+            )
+        });
+    }
+
     lista() {
         return new Promise((resolve, reject) => {
             this._db.all(
@@ -16,8 +43,9 @@ class LivroDao {
                     return resolve(resultados);
                 }
             )
-        })
-        
+        });
+
+
     }
 }
 module.exports = LivroDao;
