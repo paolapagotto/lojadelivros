0dc7805c68581955bb1a208c6fca83f3bb2c0100
paolapagotto <paolapagotto@gmail.com>
2020-09-19 16:45:11 +0100

    creating routes paramethers

--- src/app/infra/livro-dao.js
+++ src/app/infra/livro-dao.js
@@ -47,5 +47,70 @@ class LivroDao {
 
 
     }
+
+    buscaPorId(id) {
+
+        return new Promise((resolve, reject) => {
+            this._db.get(
+                `
+                    SELECT *
+                    FROM livros
+                    WHERE id = ?
+                `,
+                [id],
+                (erro, livro) => {
+                    if (erro) {
+                        return reject('NÃ£o foi possÃ­vel encontrar o livro!');
+                    }
+                    return resolve(livro);
+                }
+            );
+        });
+    }
+
+    atualiza(livro) {
+        return new Promise((resolve, reject) => {
+            this._db.run(`
+                UPDATE livros SET
+                titulo = ?,
+                preco = ?,
+                descricao = ?
+                WHERE id = ?
+            `,
+            [
+                livro.titulo,
+                livro.preco,
+                livro.descricao,
+                livro.id
+            ],
+            erro => {
+                if (erro) {
+                    return reject('NÃ£o foi possÃ­vel atualizar o livro!');
+                }
+
+                resolve();
+            });
+        });
+    }
+
+    remove(id) {
+
+        return new Promise((resolve, reject) => {
+            this._db.get(
+                `
+                    DELETE 
+                    FROM livros
+                    WHERE id = ?
+                `,
+                [id],
+                (erro) => {
+                    if (erro) {
+                        return reject('NÃ£o foi possÃ­vel remover o livro!');
+                    }
+                    return resolve();
+                }
+            );
+        });
+    }
 }
 module.exports = LivroDao;
--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -31,7 +31,22 @@ module.exports = (app) => {
     });
 
     app.get('/livros/form', function(req, resp){
-        resp.marko(require('../views/livros/form/form.marko'));    
+        resp.marko(require('../views/livros/form/form.marko'), { livro: {}});    
+    });
+
+    app.get('/livros/form/:id', function(req, resp) {
+        const id = req.params.id;
+        const livroDao = new LivroDao(db);
+    
+        livroDao.buscaPorId(id)
+            .then(livro => 
+                resp.marko(
+                    require('../views/livros/form/form.marko'),
+                    { livro: livro }
+                )
+            )
+            .catch(erro => console.log(erro));
+    
     });
 
     app.post('/livros', function(req, resp){
@@ -42,4 +57,13 @@ module.exports = (app) => {
                 .then(resp.redirect('/livros'))
                 .catch(erro => console.log(erro));
     });
+
+    app.delete('/livros/:id', (req, resp) => {
+        const id = req.params.id;
+        const livroDao = new LivroDao(db)
+        console.log('delete',id)
+        livroDao.remove(id)
+                .then(() => resp.status(200).end())
+                .catch(erro => console.log(erro));
+    })
 }
